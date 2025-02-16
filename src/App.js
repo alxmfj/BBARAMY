1c7502b126eae01b4af880c1cdf621f6dcc23afd
alxmfj <alxmfj@gmail.com>
2025-02-16 10:58:39 +0900

    App.js 수정

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>환수 최적 결속 찾기</h1>
+      <p>GitHub Pages 배포 성공! 🎉</p>
+    </div>
+  );
+}
+
+export default App;
